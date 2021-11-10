42c0d691150ebe7b6ff05eaa509133fbf08289bc
stefan <stefanace26@gmail.com>
2021-11-10 10:36:55 -0400

    Config Ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
+import '../styles/components/App.css';
 
-const App = () => {
-  return (
+const App = () => (
     <div>
       <h1>hola mundo</h1>
     </div>
-  )
-}
+  );
 
 export default App;
