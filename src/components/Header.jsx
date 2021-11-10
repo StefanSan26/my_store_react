6b6c92a8e909e674d586109db3347ac06fcb4acb
stefan <stefanace26@gmail.com>
2021-11-10 12:15:55 -0400

    Styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<div className="Header">
+			<h1 className="Header-title">PlatziConf Merch</h1>
+			<div className="Header-cheackout">
+				Checkout
+			</div>
+		</div>
+	);
+};
+
+export default Header;
