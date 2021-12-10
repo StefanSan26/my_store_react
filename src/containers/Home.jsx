6a5c8b3ae628b34038a23b682dd4cb1e131a6bb1
stefan <stefanace26@gmail.com>
2021-12-10 11:18:41 -0500

    [UPDATE] SEO

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -8,25 +8,25 @@ const Home = () => {
 		<div>
 			<Helmet>
 				<title>
-					Platzi Conf Merch - Productos
+					MY Store | React
 				</title>
 				<meta name="twitter:card" content="summary_large_image"/>
 				<meta name="twitter:site" content="@Ssteef26"/>
 				<meta name="twitter:creator" content="@Ssteef26"/>
-				<meta name="twitter:title" content="Platzi Conf Store"/>
-				<meta name="twitter:description" content="Platzi Conf Store"/>
+				<meta name="twitter:title" content="MY Store | React"/>
+				<meta name="twitter:description" content="MY Store | React"/>
 				<meta
 					name="twitter:image"
 					content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
 				/>
-				<meta property="og:title" content="Platzi Conf Store"/>
-				<meta property="og:description" content="Platzi Conf Store"/>
+				<meta property="og:title" content="MY Store | React"/>
+				<meta property="og:description" content="MY Store | React"/>
 				<meta
 					property="og:image"
 					content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
 				/>
 				<meta property="og:url" content="platzistore.xyz" />
-				<meta property="og:site_name" content="Platzi Conf Store" />
+				<meta property="og:site_name" content="MY Store | React" />
 				<meta property="og:locale" content="es_ES" />
 				<meta property="og:type" content="article" />
 				<meta property="fb:app_id" content="ID_APP_FACEBOOK" />
